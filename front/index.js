0964c0ab006189e7b9b2a2652d20001068d4957e
lidjiotyrov <lidji.ot@justiceteam-it.com>
2021-09-21 21:57:15 +0300

    update2

--- front/index.js
+++ front/index.js
@@ -15,7 +15,7 @@ const mainFunc = async () => {
   }
 
   const request2 = async () => {
-    const file = await fetch('http://localhost:3000/file1',{
+    const file = await fetch('http://localhost:3000/file2',{
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
@@ -30,63 +30,69 @@ const mainFunc = async () => {
     await request1(),
     await request2(),
   ])
+  console.log("str2",str2)
    const arrString1 = str1.split('\r').join('').split('\n').join('').split('=').join('').split('')
-   const arrString2 = str2.split('\r').join('').split('\n')
+   const arrString2 = str2.split('\r').join('').split('\n').join('').split('=').join('').split('')
+  console.log("arr2", arrString2)
 
 
-  let arrStr = []
-  let arrNum = []
-  let counter = 0
-  let counterNum = 0
-  arrString1.forEach((el)=> {
-    if(isNaN((Number(el)))) {
-      counter +=1
-    } else {
-      counter = 0
-    }
-    if(counter && counter === 1) {
-     arrStr.push(el)
-    }
-    if(counter && counter > 1) {
-      const symbol = arrStr[arrStr.length - 1] + el
-      arrStr[arrStr.length - 1] = symbol
-    }
-    if(!isNaN((Number(el)))) {
-      counterNum +=1
-    } else {
-      counterNum = 0
-    }
-    if(counterNum && counterNum === 1) {
-      arrNum.push(el)
-    }
-    if(counterNum && counterNum > 1) {
-      const symbol = arrNum[arrNum.length - 1] + el
-      arrNum[arrNum.length -1] = symbol
-    }
-  })
+  const conversionFunction = (transformArray) => {
 
-  let Obj = {}
-
-  for(let i=0; i < arrStr.length; i++) {
+    let arrStr = []
+    let arrNum = []
     let counter = 0
-    for (let j in Obj) {
+    let counterNum = 0
+    transformArray.forEach((el)=> {
+      if(isNaN((Number(el)))) {
+        counter +=1
+      } else {
+        counter = 0
+      }
+      if(counter && counter === 1) {
+        arrStr.push(el)
+      }
+      if(counter && counter > 1) {
+        const symbol = arrStr[arrStr.length - 1] + el
+        arrStr[arrStr.length - 1] = symbol
+      }
+      if(!isNaN((Number(el)))) {
+        counterNum +=1
+      } else {
+        counterNum = 0
+      }
+      if(counterNum && counterNum === 1) {
+        arrNum.push(el)
+      }
+      if(counterNum && counterNum > 1) {
+        const symbol = arrNum[arrNum.length - 1] + el
+        arrNum[arrNum.length -1] = symbol
+      }
+    })
 
-      if (j === arrStr[i]) {
-        counter = Number(Obj[j]) + Number(arrNum[i])
-        console.log("counter",counter)
+    let Obj = {}
+
+    for(let i=0; i < arrStr.length; i++) {
+      let counter = 0
+      for (let j in Obj) {
+
+        if (j === arrStr[i]) {
+          counter = Number(Obj[j]) + Number(arrNum[i])
+        }
+      }
+
+      if (counter) {
+        Obj[arrStr[i]] = counter
+      } else {
+        Obj[arrStr[i]] = +arrNum[i]
       }
     }
-
-    if (counter) {
-      Obj[arrStr[i]] = counter
-    } else {
-      Obj[arrStr[i]] = +arrNum[i]
-    }
+    return Obj
   }
-  console.log("arrStr", Obj)
-  console.log("arrNum", arrNum)
+  const Obj1 = conversionFunction(arrString1)
+  const Obj2 = conversionFunction(arrString2)
+  console.log("Obj1", Obj1)
 
-  console.log("arr2", arrString2)
+  console.log("Obj2", Obj2)
 }
 
 
