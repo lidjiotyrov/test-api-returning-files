cece73d432b131cb58c52acac98e9af2cb62da72
lidjiotyrov <lidji.ot@justiceteam-it.com>
2021-09-21 23:58:04 +0300

    update 4

--- front/index.js
+++ front/index.js
@@ -1,4 +1,6 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
+const fs = require('fs')
+
 
 const mainFunc = async () => {
 
@@ -20,6 +22,7 @@ const mainFunc = async () => {
     await requestFile(2),
   ])
 
+// Функция конверсии массива символов в общий объект файла
   const conversionFunction = (transformArray) => {
     let arrStr = []
     let arrNum = []
@@ -27,36 +30,42 @@ const mainFunc = async () => {
     let counterNum = 0
 
     transformArray.forEach((el)=> {
+
+      // Запись ключей в отдельный массив ArrStr
       if(isNaN((Number(el)))) {
         counter +=1
       } else {
         counter = 0
       }
-      if(counter && counter === 1) {
+      if(counter === 1) {
         arrStr.push(el)
       }
-      if(counter && counter > 1) {
+      if(counter > 1) {
         const symbol = arrStr[arrStr.length - 1] + el
         arrStr[arrStr.length - 1] = symbol
       }
+
+      //Запись чисел(значений) в отдельный массив ArrNum
       if(!isNaN((Number(el)))) {
         counterNum +=1
       } else {
         counterNum = 0
       }
-      if(counterNum && counterNum === 1) {
+      if(counterNum === 1) {
         arrNum.push(el)
       }
-      if(counterNum && counterNum > 1) {
+      if(counterNum > 1) {
         const symbol = arrNum[arrNum.length - 1] + el
         arrNum[arrNum.length -1] = symbol
       }
     })
 
+    //Сборка Общего объекта из ArrStr(ключи) и ArrNum(значения)
     let Obj = {}
 
     for(let i=0; i < arrStr.length; i++) {
       let counter = 0
+
       for (let j in Obj) {
 
         if (j === arrStr[i]) {
@@ -73,34 +82,52 @@ const mainFunc = async () => {
 
     return Obj
   }
+
   const Obj1 = conversionFunction(arrString1)
   const Obj2 = conversionFunction(arrString2)
-  console.log("Obj1", Obj1)
-  console.log("Obj2", Obj2)
 
-  const result = {};
+//Общий объект
+  const result = {}
 
+// Запись ключей объекта первого файла в общий объект
   Object.keys(Obj1)
-    .forEach(key => result[key] = Obj1[key]);
+    .forEach(key => result[key] = Obj1[key])
 
+// Запись ключей объекта второго файла и проверка на уникальность ключей в объектах
   Object.keys(Obj2)
     .forEach((key) => {
-
-      let counter = 0
+      let overallResult= 0
       for (let j in result) {
 
         if (j === key) {
-          counter = result[key] + Obj2[key]
+          overallResult = result[key] + Obj2[key]
         }
       }
-      if(counter) {
-        result[key] = counter
+
+      if(overallResult) {
+        result[key] = overallResult
       } else {
         result[key] = Obj2[key]
       }
     })
-  console.log("Result", result)
-}
 
+// Преобразование общего объекта в массив строк
+  const finalArrString = []
+  Object.keys(result).forEach(key => {
+    const el = key + '=' + result[key]
+    finalArrString.push(el)
+  })
+
+  const finalString = finalArrString.sort().join('\n')
+
+//Создание файла с конечным результатом
+  fs.appendFile('finalFile.txt', finalString, function (err) {
+    if (err) {
+      // append failed
+    } else {
+      // done
+    }
+  })
+}
 
 mainFunc().then()
